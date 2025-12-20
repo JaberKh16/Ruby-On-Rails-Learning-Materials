9cc7d6dae184b1f7df4b02bc0b4f2916e32b5830
Jaber Khan <md.jaberkhan66@gmail.com>
2025-12-20 15:46:52 +0600

    Rails-Basic-API-Concepts

---
+++ 3.Rails-Rest-API-Concepts/app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::API
+    before_action :authenticate_user!
+
+    skip_before_action :authenticate_user!, only: [:index]
+
+end
---
+++ 3.Rails-Rest-API-Concepts/app/controllers/books_controller.rb
@@ -0,0 +1,5 @@
+class BooksController < ApplicationController
+    def index
+        return 'books'
+    end
+end
