582720920028b28c0a921999fc3de9789c964ce0
Jaber Khan <md.jaberkhan66@gmail.com>
2026-01-08 12:04:01 +0600

    rails-scaffold-setup

--- 5.Rails-Practices-Work/simple-scaffold-practices/config/routes.rb
+++ 5.Rails-Practices-Work/simple-scaffold-practices/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  # available pages routes
   get "pages/index"
   get "pages/about"
   get "pages/contact"
   get "pages/privacy"
-  
+  get "pages/testimonials"
+  get "pages/services"
+
 end
+0
